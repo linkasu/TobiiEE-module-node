17ff50a458404b98c5fba96f0b5cdc09c4730263
Ivan Bakaidov <ivan@aacidov.ru>
2021-07-11 18:34:30 +0300

    UPDATE

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,11 @@
-import { ProcessListener } from "./ProcessListener";
-export declare const TobiiProcess: typeof ProcessListener;
+/// <reference types="node" />
+import { EventEmitter } from "events";
+export declare class TobiiProcess extends EventEmitter {
+    process: import("child_process").ChildProcessWithoutNullStreams | null;
+    constructor();
+    private get binPath();
+    start(): void;
+    private closeHandler;
+    private chunkHandler;
+}
 //# sourceMappingURL=index.d.ts.map
